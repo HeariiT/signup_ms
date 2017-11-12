8f8ebfa3df104c2e05003b24bb4c1e25ac99c80b
David Guzmán <djguzmanc@unal.edu.co>
2017-11-12 16:41:50 -0500

    General Commit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker-compose build
-docker-compose run --rm signup_ms rails db:create
-docker-compose run --rm signup_ms rails db:migrate
+docker-compose run --rm signup-ms rails db:create
+docker-compose run --rm signup-ms rails db:migrate
 docker-compose up
